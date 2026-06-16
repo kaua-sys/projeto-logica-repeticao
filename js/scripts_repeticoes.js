f728d18f8aa3d1843c9206834b8e4315f0a135d3
Luiz Kauã <lkaua4546@gmail.com>
2026-06-16 16:32:59 -0300

    Criado divfor.html

--- js/scripts_repeticoes.js
+++ js/scripts_repeticoes.js
@@ -1,5 +1,6 @@
 //Estrutura de Repetição for
 const divFor= document.querySelector('#div-for')
 for(i = 0; i <= 10; i++) {
-    console.log(i)
+    console.log(i,'amor')
+    divFor.innerHTML += `${i} - Amor <br> `
 }
